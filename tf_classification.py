1707a97b56060ed7177f5eee35a0caf2bc80dcbe
YaoLu <luyao777@vip.qq.com>
2017-09-30 15:51:36 +0800

    debug

--- tf_classification.py
+++ tf_classification.py
@@ -13,19 +13,21 @@ def add_layer(inputs, in_size, out_size, activation_function = None):
     else:
         output = activation_function(Wx_plus_b)
     return output
+
 def compute_accuracy(v_xs, v_ys):
     global prediction
     y_pre = sess.run(prediction, feed_dict = {xs: v_xs})
-    correct_prediction = tf.equal(tf.argmax(y_pre, 1). tf.argmax(v_ys, 1))
+    correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
     return result
+
 xs = tf.placeholder(tf.float32, [None, 784])
-ys = tf.placeholder(tf.flaoat32, [None, 10])
+ys = tf.placeholder(tf.float32, [None, 10])
 
 prediction = add_layer(xs, 784, 10, activation_function = tf.nn.softmax)
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),reduction_indices[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices = [1]))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
